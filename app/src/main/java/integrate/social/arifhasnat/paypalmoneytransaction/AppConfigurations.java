c32d759de05efef6ece27e55d6bfe003a263d6c3
arifhasnat <arifhasnat.info@gmail.com>
2016-10-06 14:39:14 +0600

    paypal integration android

---
+++ app/src/main/java/integrate/social/arifhasnat/paypalmoneytransaction/AppConfigurations.java
@@ -0,0 +1,9 @@
+package integrate.social.arifhasnat.paypalmoneytransaction;
+
+/**
+ * Created by arif hasnat on 10/6/2016.
+ */
+public class AppConfigurations {
+    public static String paypal_client_id= "AbaKJRRCZs9VbmZbx8eiq6Xu5pgFSIYrxD5Q_Y16GmbXXvYyfWy-ApQMTF6B1lPtty4d20P0Lf1AOw9U";
+
+}
